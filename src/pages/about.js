c277376736af2fb79b84c7501375e8206b367406
fitra-nurakbar <fitranurakbar378@gmail.com>
2022-07-23 22:05:01 +0700

    Components Navbar

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react"
+import Navbar from "../components/Navbar"
 
 export default function About() {
   return (
-    <div>about</div>
+    <div className="">
+     <Navbar />
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+        reiciendis sed quam maxime fugit eum voluptatem distinctio impedit
+        fugiat, enim sapiente veniam doloribus provident adipisci itaque. Nam
+        quas ad voluptatem.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+        reiciendis sed quam maxime fugit eum voluptatem distinctio impedit
+        fugiat, enim sapiente veniam doloribus provident adipisci itaque. Nam
+        quas ad voluptatem.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+        reiciendis sed quam maxime fugit eum voluptatem distinctio impedit
+        fugiat, enim sapiente veniam doloribus provident adipisci itaque. Nam
+        quas ad voluptatem.
+      </p>
+    </div>
   )
 }
---
+++ src/pages/projects/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Navbar from "../../components/Navbar"
+
+export default function Project() {
+  return (
+    <div>
+     <Navbar />
+      <h1>Project</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+        dolorum dolore deleniti dolores illo laborum, aspernatur quae
+        necessitatibus, impedit non natus recusandae adipisci possimus fugit
+        earum. Labore quasi similique odit?
+      </p>
+    </div>
+  )
+}
